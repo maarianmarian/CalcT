0a3fb35bd994f779587f42915aa3b8e6dfd5acdc
Mariana Ruiz <cadenitadioro@gmail.com>
2022-10-27 19:09:29 -0500

    dias de vacaciones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-
 import dignas from './img/dignas.jpg';
 import './App.css';
-
 import React, {Component} from 'react';
 
 
@@ -16,14 +14,14 @@ class App extends Component {
     fecha1: "sin fecha",
     fecha2: "sin fecha",
     fechaRes: "",
-    aniosantig: "",
+    antig: "",
     sueldo: "",
     vacaciones: "",
     aguinaldo: "",
     finiquito: ""
   }
 
-
+//definición de fechas
   dateConverter = () => {
     const newStartDate = new Date(this.state.fecha1);
     const newEndDate = new Date(this.state.fecha2);
@@ -36,9 +34,7 @@ class App extends Component {
     } else {
       return result
     }
-
   }
-
 
   changeState = () => {
 
@@ -49,49 +45,70 @@ class App extends Component {
     });
 
   }
+  //días, años y sueldo
   calcular = () => {
     let sueldoDiario = 0;
     let vacaciones = 0;
     let aguinaldo = 0;
     let finiq = 0;
     let RestaFechas = new Date();
-    let aniosantig = 0
+    let antig = 0;
+    let dAntig = 0;
+
 
     RestaFechas = this.dateConverter();
-    //  A QUI SE CAMBIAN LOS PORCENTAJES
+    //  A QUI SE CAMBIAN LOS PORCENTAJES QUE TE DIGAN
     if (this.state.sueldo > 0 && this.state.sueldo !== undefined) {
       sueldoDiario = (this.state.sueldo / 7);
-      vacaciones = ((RestaFechas) * ((sueldoDiario / 100)) * 5).toFixed(2);
-      finiq = ((RestaFechas) * ((sueldoDiario / 100)) * 10).toFixed(2);
-      aguinaldo = ((RestaFechas) * ((sueldoDiario / 100)) * 15).toFixed(2);
-      aniosantig = (RestaFechas).toFixed(0);
+      antig = Math.trunc(RestaFechas / 365);
 
-      this.setState({
-        fechaRes: RestaFechas,
-        sueldo: this.state.sueldo,
-        sualdodiario: sueldoDiario,
-        vacaciones: vacaciones,
-        finiquito: finiq,
-        aguinaldo: aguinaldo,
-        aniosantig: aniosantig,
+//calculo pago vacaciones
+     // dAntig = Math.trunc(RestaFechas / 365);
 
-      });
-    }
-    console.log('sueldo semanal ' + this.state.sueldo);
-    console.log('sueldo diario' + this.state.sualdodiario);
-    console.log('dias laborados' + this.state.fechaRes);
-    console.log('años de antigüedad' + this.state.aniosantig);
-    console.log(' vacaciones ' + this.state.vacaciones);
-    console.log('finiquito ' + this.state.finiquito);
-    console.log('aguinaldo' + this.state.aguinaldo);
-    console.log('vacas' + this.state.vacaciones);
+      if (this.state.antig > 0 && this.state.antig !== undefined) {
+          vacaciones = 0;}
+      if (antig = 1) {
+          vacaciones = 132;
+       }
+      if (antig = 2) {
+          vacaciones = 8;
+         }
+      if (antig = 3) {
+          vacaciones = 10;
+           }
+      if (antig = 4) {
+          vacaciones = 12;
+             }
 
-  }
+             //calculo días vacaciones if (this.antig = 0 && this.vacaciones !== 4)
 
+             finiq = ((RestaFechas) * ((sueldoDiario / 100)) * 10).toFixed(2);
+             aguinaldo = ((RestaFechas) * ((sueldoDiario / 100)) * 15).toFixed(2);
+             this.setState({
+               fechaRes: RestaFechas,
+               sueldo: this.state.sueldo,
+               sualdodiario: sueldoDiario,
+               antig: antig,
+               vacaciones: vacaciones,
+               finiquito: finiq,
+               aguinaldo: aguinaldo,
+             });
+           }
+
+           console.log('sueldo semanal ' + this.state.sueldo);
+           console.log('sueldo diario' + this.state.sualdodiario);
+           console.log('dias laborados' + this.state.fechaRes);
+           console.log(' antigüedad ' + this.state.antig);
+           console.log(' vacaciones ' + this.state.vacaciones);
+           console.log('finiquito ' + this.state.finiquito);
+           console.log('aguinaldo' + this.state.aguinaldo);
+           console.log('vacas' + this.state.vacaciones);
+
+         }
   render() {
     return (
-
         <>
+
 
           <div className="center">
 
@@ -106,11 +123,12 @@ class App extends Component {
               <input type="date" ref={this.fecha1} onChange={this.changeState}></input><br/><br/>
               <label>Fecha de salida </label><br/><br/>
               <input type="date" ref={this.fecha2} onChange={this.changeState}></input><br/><br/>
+
               <button className="btn" id="btn-table" onClick={this.calcular}>Calcular</button>
               <br/><br/>
               <output>Días días trabajados: {this.state.fechaRes}</output>
               <br/>
-              <output> Años de antigüedad: {this.state.aniosantig}</output>
+              <output>Antigüedad: {this.state.antig}</output>
               <br/>
               <output>Vacaciones: {this.state.vacaciones}</output>
               <br/>

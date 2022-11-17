966f4c162232478d6dde3be6864fc9778052a49d
Mariana Ruiz <cadenitadioro@gmail.com>
2022-11-17 12:15:53 -0600

    sin licencias

--- src/App.js
+++ src/App.js
@@ -1,173 +1,93 @@
-import dignas from './img/dignas.jpg';
 import './App.css';
 import React, {Component} from 'react';
+import Bar from "./components/Bar";
+import CalcT from "./components/CalcT";
+import Vacas from "./components/Vacas";
+import Agui from "./components/Agui";
+import Finiq from "./components/finiq";
 
 
-class App extends Component {
-  fecha1 = React.createRef();
-  fecha2 = React.createRef();
-  fechaResult = React.createRef();
-  fechaResult = "sin resultado";
-  sueldoRef = React.createRef();
-
-  state = {
-    fecha1: "sin fecha",
-    fecha2: "sin fecha",
-    fechaRes: "",
-    antig: "",
-    sueldo: "",
-    vacaciones: "",
-    aguinaldo: "",
-    finiquito: ""
-  }
-
-//definición de fechas
-  dateConverter = () => {
-    const newStartDate = new Date(this.state.fecha1);
-    const newEndDate = new Date(this.state.fecha2);
-    const one_day = 1000 * 60 * 60 * 24;
-    let result = "";
-    result = Math.ceil((newEndDate.getTime() - newStartDate.getTime()) / (one_day))
-    // console.log('math result'  + result);
-    if (result < 0) {
-      return 0
-    } else {
-      return result
+class App extends Component
+{
+    UNSAFE_componentWillMount() {
+        this.setState({view: 1
+    })
     }
-  }
 
-  changeState = () => {
-
-    this.setState({
-      fecha1: this.fecha1.current.value,
-      fecha2: this.fecha2.current.value,
-      sueldo: this.sueldoRef.current.value
-    });
-
-  }
-  //días, años y sueldo
-  calcular = () => {
-    let sueldoDiario = 0;
-    let vacaciones = 0;
-    let aguinaldo = 0;
-    let finiq = 0;
-    let RestaFechas = 0;
-    let antig = 0;
-
-
-    RestaFechas = this.dateConverter();
-    //  A QUI SE CAMBIAN LOS PORCENTAJES QUE TE DIGAN
-    if (this.state.sueldo > 0 && this.state.sueldo !== undefined) {
-      sueldoDiario = (this.state.sueldo / 7);
-      antig = Math.trunc(RestaFechas / 365);
-    console.log (antig)
-
-
-//calculo pago vacaciones
-     // dAntig = Math.trunc(RestaFechas / 365);
-
-      if (this.state.antig > 0 && this.state.antig !== undefined) {
-          vacaciones = 0;}
-      if (antig === 1) {
-          vacaciones = 132;
-       }
-      if (antig === 2) {
-          vacaciones = 8;
-         }
-      if (antig === 3) {
-          vacaciones = 10;
-           }
-      if (antig === 4) {
-          vacaciones = 12;
-             }
-      if (antig >= 5 || antig <= 9) {
-            vacaciones = 14;
+    ChangeView1 =() =>{
+        this.setState({view: 1
+        })
+        console.log(this.state.view)
+    }
+    ChangeView2 =() =>{
+        this.setState({view: 2
+        })
+        console.log(this.state.view)
+    }
+    ChangeView3 =() =>{
+        this.setState({view: 3
+        })
+        console.log(this.state.view)
+    }
+    ChangeView4 =() =>{
+        this.setState({view: 4
+        })
+        console.log(this.state.view)
+    }
+    render() {
+        if(this.state.view == 1){
+            return (
+                <>
+                    <Bar
+                        ChangeView1 = {this.ChangeView1}
+                        ChangeView2 = {this.ChangeView2}
+                        ChangeView3 = {this.ChangeView3}
+                        ChangeView4 = {this.ChangeView4}
+                    />
+                    <CalcT/>
+                    </>
+            )
         }
-      if (antig >= 10 || antig <= 14) {
-            vacaciones = 16;
+        if(this.state.view == 2){
+            return (
+                <>
+                    <Bar
+                        ChangeView1 = {this.ChangeView1}
+                        ChangeView2 = {this.ChangeView2}
+                        ChangeView3 = {this.ChangeView3}
+                        ChangeView4 = {this.ChangeView4}
+                    />
+                    <Vacas/>
+                </>
+            )
         }
-      if (antig >= 15 || antig <= 19) {
-            vacaciones = 18;
+        if(this.state.view == 3){
+            return (
+                <>
+                    <Bar
+                        ChangeView1 = {this.ChangeView1}
+                        ChangeView2 = {this.ChangeView2}
+                        ChangeView3 = {this.ChangeView3}
+                        ChangeView4 = {this.ChangeView4}
+                    />
+                    <Agui/>
+                </>
+            )
         }
-      if (antig >= 21 || antig <= 24) {
-            vacaciones = 20;
+        if(this.state.view == 4){
+            return (
+                <>
+                    <Bar
+                        ChangeView1 = {this.ChangeView1}
+                        ChangeView2 = {this.ChangeView2}
+                        ChangeView3 = {this.ChangeView3}
+                        ChangeView4 = {this.ChangeView4}
+                    />
+                    <Finiq/>
+                </>
+            )
         }
-      if (antig >= 25 || antig <= 29) {
-            vacaciones = 22;
-        }
-      if (antig >= 30 || antig <= 34) {
-            vacaciones = 24;
-        }
-      if (antig >= 35 || antig <= 39) {
-            vacaciones = 26;
-        }
-      if (antig >= 40 || antig <= 44) {
-            vacaciones = 28;
-        }
-      if (antig >= 45 || antig <= 49) {
-            vacaciones = 30;
-        }
+    }
 
-
-             //calculo días vacaciones if (this.antig = 0 && this.vacaciones !== 4)
-
-             finiq = ((RestaFechas) * ((sueldoDiario / 100)) * 10).toFixed(2);
-             aguinaldo = ((RestaFechas) * ((sueldoDiario / 100)) * 15).toFixed(2);
-             this.setState({
-               fechaRes: RestaFechas,
-               sueldo: this.state.sueldo,
-               sualdodiario: sueldoDiario,
-               antig: antig,
-               vacaciones: vacaciones,
-               finiquito: finiq,
-               aguinaldo: aguinaldo,
-             });
-           }
-
-           console.log('sueldo semanal ' + this.state.sueldo);
-           console.log('sueldo diario' + this.state.sualdodiario);
-           console.log('dias laborados' + this.state.fechaRes);
-           console.log(' antigüedad ' + this.state.antig);
-           console.log(' vacaciones ' + this.state.vacaciones);
-           console.log('finiquito ' + this.state.finiquito);
-           console.log('aguinaldo' + this.state.aguinaldo);
-           console.log('vacas' + this.state.vacaciones);
-
-         }
-  render() {
-    return (
-        <>
-
-
-          <div className="center">
-
-            <div id="sidebar" className="archivosAdminCenter3">
-              <div>
-                <img src={dignas}/>
-              </div>
-              <h1>Dignas</h1><br/><br/>
-              <label>Sueldo semanal</label><br/><br/>
-              <input type="number" ref={this.sueldoRef} onChange={this.changeState}></input><br/><br/>
-              <label>Fecha de ingreso </label><br/><br/>
-              <input type="date" ref={this.fecha1} onChange={this.changeState}></input><br/><br/>
-              <label>Fecha de salida </label><br/><br/>
-              <input type="date" ref={this.fecha2} onChange={this.changeState}></input><br/><br/>
-              <button className="btn" id="btn-table" onClick={this.calcular}>Calcular</button>
-              <br/><br/>
-              <output>Días días trabajados: {this.state.fechaRes}</output>
-              <br/> <br/>
-              <output>Antigüedad: {this.state.antig}</output>
-              <br/> <br/>
-              <output>Vacaciones: {this.state.vacaciones}</output>
-              <br/> <br/>
-              <output>Aguinaldo:{this.state.aguinaldo}</output>
-              <br/> <br/>
-              <output>Finiquito:{this.state.finiquito}</output>
-              <br/> <br/>
-            </div>
-          </div>
-        </>
-    );
-  }
 }
 export default App;
--- src/components/Bar.js
+++ src/components/Bar.js
@@ -2,41 +2,39 @@ import React, { Component } from 'react';
 
 class Bar extends Component {
 
-    state = {
-       
-        nombre: "ok"
-    }
-
-    cerrarSesion() {
-        
-        window.location.reload();
-    }
-
     render() {
-       {/* const {changeViewLogin} = this.props   
-        const {changeViewHome} = this.props
-        const {changeViewCreate} = this.props  
-        const {changeViewDirectorio} = this.props  
-    const {changeViewMiDirectorio} = this.props     */} 
-        return (
-            <tbody >
+        const {ChangeView1} = this.props
+        const {ChangeView2} = this.props
+        const {ChangeView3} = this.props
+        const {ChangeView4} = this.props
+
+        return(
+            <div>
+                <tbody >
                 <tr >
-                    <th  >
+                    <th className="table" >
                         <div >
-                            <ul>
-
-
-
-
-                            </ul>
+                                <ul>
+                                    <li>
+                                        <div>
+                                            <button className="btn-navBar" onClick={ChangeView1}>Completo</button>
+                                            <button className="btn-navBar" onClick={ChangeView2}>Vacaciones</button>
+                                            <button className="btn-navBar" onClick={ChangeView3}>Aguinaldo</button>
+                                            <button className="btn-navBar" onClick={ChangeView4}>Finiquito</button>
+                                        </div>
+                                    </li>
+                                </ul>
                         </div>
 
                     </th>
                 </tr>
-            </tbody>
-           
-           
+                </tbody>
+                <div>
 
+
+                </div>
+
+            </div>
         );
     }
 
